ec8f68ee09a01bb8ffcdae46080820d88cbfa7d9
m-dantas <mauricio.dantascp@gmail.com>
2024-08-26 09:59:32 -0300

    style: mescla estilos do Meterial Design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { IndexComponent } from './pages/index/index.component';
 export const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent
+    },
     {
         path: 'login',
         component: LoginComponent
